93e4c03f19a959d19290bbb9ef7b29e196b3aa07
Alex <sasa.stecova@gmail.com>
2021-03-03 20:16:19 +0100

    fixed burger options css

--- src/containers/BurgerBuilder/Burger/Burger.js
+++ src/containers/BurgerBuilder/Burger/Burger.js
@@ -4,16 +4,25 @@ import BurgerIngredient from "../BurgerIngredient/BurgerIngredient";
 import classes from "./Burger.module.css";
 
 const burger = (props) => {
+  let newBurger = true
   const ingredients = Object.keys(props.ingredients).map((item) => {
     return [...Array(props.ingredients[item])].map((_, key) => {
       return <BurgerIngredient key={item + key} type={item} />;
     });
   });
+  
+  Object.keys(props.ingredients).some((item) => {
+    console.log(item, props.ingredients[item])
+    if (props.ingredients[item]> 0){
+      newBurger = false;
+      return
+    }
+  })
 
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      {ingredients}
+      {newBurger ? <p className={classes.Info} >Make it juicy!</p> : ingredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- src/containers/BurgerBuilder/BurgerOptions/Options/BurgerOption.js
+++ src/containers/BurgerBuilder/BurgerOptions/Options/BurgerOption.js
@@ -7,15 +7,21 @@ const burgerOption = (props) => {
     <div className={classes.Option}>
       <label className={classes.Label}>{props.label}</label>
       <div className={classes.OptionButtons}>
-      <button
-          className={classes.LessButton}
+        <button
+          className={[classes.LessButton, classes.OptionButton].join(" ")}
           onClick={props.removed}
           disabled={props.disabled}
         >
-          Less
+          <span className={classes.ButtonLabel}>Less</span>
+          <span className={classes.ButtonIcon}>&#x2212;</span>
         </button>
-        <button className={classes.MoreButton} onClick={props.added}>
-          More
+        <button
+          type
+          className={[classes.MoreButton, classes.OptionButton].join(" ")}
+          onClick={props.added}
+        >
+          <span className={classes.ButtonLabel}>More</span>
+          <span className={classes.ButtonIcon}>&#65291;</span>
         </button>
       </div>
     </div>
