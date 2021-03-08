5f77ee9ef21fc37231404d00d3382e4ce3b157f6
Alex <sasa.stecova@gmail.com>
2021-03-08 18:00:57 +0100

    add cancel symbol in modal

--- src/containers/BurgerOptions/OrderSummary/OrderSummary.js
+++ src/containers/BurgerOptions/OrderSummary/OrderSummary.js
@@ -1,35 +1,36 @@
 import React from "react";
 
-import modalclasses from '../../../Layout/UI/Modal/Modal.module.css'
+import classes from './OrderSummary.module.css'
 
 const orderSummary = (props) => (
   <div>
-    <div className={modalclasses.ModalLabel}>This is a burger detail</div>
+    <div className={classes.Title}>Smack'dat</div>
     <ul>
       {Object.keys(props.ingredients).map((item) => {
         return <li key={item}>{item}</li>;
       })}
     </ul>
 
-    <p className={modalclasses.ModalLabel}>
+    <p className={classes.ModalLabel}>
       Total price: <strong>{props.totalPrice.toFixed(2)}</strong>
     </p>
-    <div className={modalclasses.ModalButton}>
+    <div className={classes.ModalButton}>
+
       <button
-        className={[modalclasses.CancelButton, modalclasses.ModalButton].join(
-          " "
-        )}
-        onClick={props.cancelPurchaseHandler}
-      >
-        Cancel
-      </button>
-      <button
-        className={[modalclasses.OrderButton, modalclasses.ModalButton].join(
+        className={[classes.OrderButton, classes.ModalButton].join(
           " "
         )}
         onClick={props.orderBurgerHandler}
       >
         Order
+      </button>
+      <button
+        className={[classes.CancelButton, classes.ModalButton].join(
+          " "
+        )}
+        onClick={props.cancelPurchaseHandler}
+      >
+        Cancel
       </button>
     </div>
   </div>
