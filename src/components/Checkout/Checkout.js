17b38f2720c9bc796dfc92b0a1f944443dbafce0
Alex <sasa.stecova@gmail.com>
2021-03-08 16:08:37 +0100

    css burger hotfix

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,9 +1,21 @@
-import React from 'react'
-import OrderSummary from "../../containers/BurgerOptions/OrderSummary/OrderSummary"
+import React, { Component } from "react";
+import Burger from "../../containers/Burger/Burger";
+import OrderSummary from "../../containers/BurgerOptions/OrderSummary/OrderSummary";
 
-const checkout = (props)=>(
-  <h1>This is checkout</h1>
-  // <OrderSummary>
-)
+class Checkout extends Component {
+  state = {
+    ingredients: { meat: 1, salad: 1, bacon: 1, cheese: 1 },
+    totalPrice: 20
+  };
 
-export default checkout;
+  render() {
+    return(
+      <div>
+        <Burger ingredients={this.state.ingredients}/>
+        <OrderSummary ingredients={this.state.ingredients} totalPrice={this.state.totalPrice}/>;
+      </div>
+    ) 
+  }
+}
+
+export default Checkout;
