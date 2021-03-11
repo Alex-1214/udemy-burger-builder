5666ad298c632bb4682e5e4636c1f6d3e871469e
Alex-1214 <43934976+Alex-1214@users.noreply.github.com>
2021-03-11 15:38:33 +0100

    Feature/my order (#15)
    * create order cards
    
    * horizontal alignment of cards

--- src/components/MyOrders/MyOrders.js
+++ src/components/MyOrders/MyOrders.js
@@ -1,9 +1,44 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
+import axios from "../../axios-burger";
+import Card from "../../Layout/UI/Card/Card";
 
-class MyOrders extends Component{
-  render(){
-    return <h1 style={{marginTop: "130px"}}>My order page</h1>
+import classes from "./MyOrders.module.css";
+
+class MyOrders extends Component {
+  state = {
+    orders: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get("/orders.json")
+      .then((response) => {
+        let orders = [];
+        Object.keys(response.data).map((key) => {
+          orders.push({ ...response.data[key], id: key });
+        });
+        this.setState({
+          orders: orders,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    let orders = this.state.orders.map((order) => <Card ordered={order.ordered} ingredients={order.ingredients} id={order.id} />);
+    return (
+      <div className={classes.MyOrders}>
+        <h2>Your Burgers</h2>
+        <h4>This week</h4>
+        <div className={classes.Cards}>
+        {orders}
+        </div>
+        
+      </div>
+    );
   }
 }
 
-export default MyOrders
+export default MyOrders;
